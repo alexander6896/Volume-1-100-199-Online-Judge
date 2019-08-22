7e27ae7e0d72e044b590a6bddc5bd90dc47427f0
alexander6896 <43659485+alexander6896@users.noreply.github.com>
2019-08-22 01:50:09 -0500

    Add files via upload

---
+++ 100solve.py
@@ -0,0 +1,20 @@
+def problema(numero):
+    int(numero)
+    if(numero==1):
+        print (numero)
+        return 0
+    elif(numero%2!=0):
+        numero=3*numero+1
+        print(numero)
+        problema(numero)
+    elif(numero%2==0):
+        numero=numero/2
+        print(numero)
+        problema(numero)
+      
+#FUNCION MAIN PARA EJECUTAR LA FUNCION RECURSIVA
+if __name__ == "__main__":
+    #print("Numero por ingresar")
+    #ingreso=input()
+    #numero=int(ingreso)
+    problema(int(22))
